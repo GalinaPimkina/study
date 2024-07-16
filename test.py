45cb7cd59637a9c26698a193d9acd04e0b98b634
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-07-16 16:52:36 +0300

    kata done

---
+++ test.py
@@ -0,0 +1,23 @@
+# def multy(arr):
+#     mul = 1
+#     for i in arr:
+#         mul *= i
+#     return mul
+#
+# def persistence(n):
+#     count = 0
+#     while n >= 10:
+#         lst = []
+#         for i in str(n):
+#             lst.append(int(i))
+#         n = multy(lst)
+#         count += 1
+#     print(count)
+#     return count
+#
+# persistence(4)
+
+
+# 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit, there are 3 multiplications)
+# ----------------------------------------------------------------------------------------------------------------------
+
