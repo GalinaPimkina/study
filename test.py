e6505487657f782699e5342623049b6d648f176a
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-08-01 15:54:02 +0300

    4 kata +

--- test.py
+++ test.py
@@ -165,7 +165,34 @@ from functools import reduce
 #     return summ
 #
 # print(sequence_sum(2, 2, 2)) #12
+#------------------------------------------------------------------------
+# Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+#
+# Examples:
+# input:    output:
+# 0    ->   0
+# 2    ->   5
+# 3    ->   5
+# 12   ->   15
+# 21   ->   25
+# 30   ->   30
+# -2   ->   0
+# -5   ->   -5
+# etc.
+# Input may be any positive or negative integer (including 0).
+# You can assume that all inputs are valid integers.
 
+# def round_to_next5(n):
+#     if -4 <= n <= 0:
+#         return 0
+#     elif 0 < n <= 5:
+#         return 5
+#     elif n % 5 == 0:
+#         return n
+#     else:
+#         return (n // 5 + 1) * 5
+#
+# print(round_to_next5(12))
 
 
 
