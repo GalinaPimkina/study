cb6dbb82ae0c9660330db3efe9bb6f63c581994c
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-08-01 16:39:53 +0300

    8 kata +

--- test.py
+++ test.py
@@ -231,9 +231,19 @@ from functools import reduce
 #     return sorted(list(test.lower())) == sorted(list(original.lower()))
 #
 # print(is_anagram("apple", "pale"))
+# -------------------------------------------------------------------
 
+# Write a function that takes a single non-empty string of only lowercase and uppercase
+# ascii letters (word) as its argument, and returns an ordered list containing
+# the indices of all capital (uppercase) letters in the string.
+# Example (Input --> Output)
+#
+# "CodEWaRs" --> [0,3,4,6]
 
-
+# def capitals(word):
+#     return [i for (i, c) in enumerate(word) if c.isupper()]
+#
+# print(capitals('PSOBpmOcx')) # [0, 1, 2, 3, 6]
 
 
 
