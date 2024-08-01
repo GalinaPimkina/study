107b2d8bbfb9c75ed32f4a59fcaa5c214bc979a5
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-08-01 16:02:11 +0300

    5 kata +

--- test.py
+++ test.py
@@ -194,9 +194,18 @@ from functools import reduce
 #
 # print(round_to_next5(12))
 
+#---------------------------------------------------------------------
+# Finish the solution so that it sorts the passed in array of numbers. If the function
+# passes in an empty array or null/nil value then it should return an empty array.
+#
+# For example:
+# solution([1,2,3,10,5]) # should return [1,2,3,5,10]
+# solution(None) # should return []
 
-
-
+# def solution(nums):
+#     return [] if not nums else sorted(nums)
+#
+# print(solution(None))
 
 
 
