7698377277090cb895c4f6f7664fde4a28f62455
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-08-02 15:01:27 +0300

    4 kata +

--- test.py
+++ test.py
@@ -313,7 +313,13 @@ from functools import reduce
 
 # ----------------------------------------------------------------------------------------------
 
-
-
-
-
+# Given a string str, reverse it and omit all non-alphabetic characters.
+# Example
+# For str = "krishan", the output should be "nahsirk".
+# For str = "ultr53o?n", the output should be "nortlu".
+#
+# def reverse_letter(st):
+#     return "".join([i for i in list(st) if i.isalpha()])[::-1]
+#
+# print(reverse_letter("ultr53o?n"))  #"nortlu"
+#---------------------------------------------------------------------------------------------------------------
