2d1e38bddab4458378ca1b9c8b58acddc86da950
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-08-02 14:16:14 +0300

    2 kata +

--- test.py
+++ test.py
@@ -255,9 +255,20 @@ from functools import reduce
 #     return len(array) == len([i for i in array if i <= limit])
 #
 # print(small_enough([78, 117, 110, 99, 104, 117, 107, 115], 100))
+
 # ------------------------------------------------------------------------------------------------
+# The two oldest ages function/method needs to be completed. It should take an array of numbers as
+# its argument and return the two highest numbers within the array. The returned value should be
+# an array in the format [second oldest age,  oldest age].
+#
+# The order of the numbers passed in could be any order. The array will always include at least
+# 2 items. If there are two or more oldest age, then return both of them in array format.
 
-
+# def two_oldest_ages(ages):
+#     return sorted(ages)[-2::]
+#
+# print(two_oldest_ages([1, 5, 87, 45, 8, 8])) # [45, 87]
+# --------------------------------------------------------------------------------
 
 
 
