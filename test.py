b35bc8444b2c16834b7bb7ba8b8842841ab30176
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-08-02 14:05:51 +0300

    1 kata +

--- test.py
+++ test.py
@@ -245,7 +245,17 @@ from functools import reduce
 #
 # print(capitals('PSOBpmOcx')) # [0, 1, 2, 3, 6]
 
+# -------------------------------------------------------------------------------------------
 
+# You will be given an array and a limit value. You must check that all values in the array
+# are below or equal to the limit value. If they are, return true. Else, return false.#
+# You can assume all values in the array are numbers.
+
+# def small_enough(array, limit):
+#     return len(array) == len([i for i in array if i <= limit])
+#
+# print(small_enough([78, 117, 110, 99, 104, 117, 107, 115], 100))
+# ------------------------------------------------------------------------------------------------
 
 
 
